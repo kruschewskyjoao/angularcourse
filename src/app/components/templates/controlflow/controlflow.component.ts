e9e8cd67fbfe160c8ae91031688ef29f2d0c5576
joaokruschewsky <joaokruschewsky@gmail.com>
2024-07-24 19:45:16 -0300

    if, for, for-empty, switchcase

--- src/app/components/templates/controlflow/controlflow.component.ts
+++ src/app/components/templates/controlflow/controlflow.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable, delay, of } from "rxjs";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
-  selector: 'app-controlflow',
+  selector: "app-controlflow",
   standalone: true,
-  imports: [],
-  templateUrl: './controlflow.component.html',
-  styleUrl: './controlflow.component.scss'
+  imports: [AsyncPipe],
+  templateUrl: "./controlflow.component.html",
+  styleUrl: "./controlflow.component.scss",
 })
 export class ControlflowComponent {
   public isTrue = true;
+  public switchCondition = "B";
+  public itens = [
+    {
+      name: "Dener troquatte",
+    },
+  ];
+
+  public loadingData$: Observable<string[]> = of([
+    "Item 1",
+    "Item 2",
+    "Item 3",
+  ]).pipe(delay(3000));
+
+  public addNewName(value: string) {
+    return this.itens.push({ name: value });
+  }
 }
