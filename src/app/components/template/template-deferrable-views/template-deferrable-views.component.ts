a2daad37cb168d46f3ef1d10dcb46e8bd6cc658a
joaokruschewsky <joaokruschewsky@gmail.com>
2024-07-29 20:16:14 +0000

    deferrable views finish

--- src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
+++ src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NewComponentComponent } from "../../new-component/new-component.component";
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-deferrable-views',
@@ -9,5 +10,18 @@ import { NewComponentComponent } from "../../new-component/new-component.compone
   styleUrl: './template-deferrable-views.component.scss'
 })
 export class TemplateDeferrableViewsComponent {
+  public isTrue = false;
 
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+    'item 4',
+    'item 5',
+    'item 6',
+    'item 7',
+    'item 8',
+    'item 9',
+    'item 10'
+  ]).pipe(delay(3000))
 }
